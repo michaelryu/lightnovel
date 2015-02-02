8672704f060fb7546ec6d0a6795825a39794289f
Michael Ryu <michaelryu0@gmail.com>
2015-02-01 17:34:27 -0800

    basic user login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+	devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+		get '/users/:id/edit', to: 'users/registrations#edit'
+    match '/users/:id', :to => 'users#show', :as => :user, :via => :get
+  end
+  
   root "pages#home"
 end
