bc5ba9c60b984560f0e784602837aa5c5ffc7e0c
Michael Ryu <michaelryu0@gmail.com>
2015-02-06 21:47:56 -0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205001956) do
+ActiveRecord::Schema.define(version: 20150206005737) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20150205001956) do
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
     t.string   "name"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
