b9a268cf30171d4317c403f28bbf9e6e10bcefb0
Michael Ryu <michaelryu0@gmail.com>
2015-02-08 17:25:21 -0800

    designed home for non users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
   	@user = current_user
+    @unovel=  Novel.includes(:chapters).order("chapters.created_at desc") 
     if user_signed_in?
   	 @updates = @user.get_voted Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 1)
     end
