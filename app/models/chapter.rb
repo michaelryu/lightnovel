03d8adf6e16078985ab39b2511d72726788ea156
Michael Ryu <michaelryu0@gmail.com>
2015-02-10 15:09:32 -0800

    changed ordering with touch: true to chapter

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,5 +1,5 @@
 class Chapter < ActiveRecord::Base
-  belongs_to :novel
+  belongs_to :novel, touch: true
   belongs_to :user
   validates :title, presence: true, length: { maximum: 140 }
   validates :content, presence: true, length: { minimum: 140 }
