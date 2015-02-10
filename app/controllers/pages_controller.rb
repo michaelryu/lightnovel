23d8212651ffbff4d9987434ba7bb5fe5f7e92a4
Michael Ryu <michaelryu0@gmail.com>
2015-02-09 20:13:55 -0800

    changed back variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,13 @@ class PagesController < ApplicationController
   def home
     @unovel=  Novel.includes(:chapters).order("chapters.created_at desc") 
     if user_signed_in?
-  	 @updated = current_user.get_voted Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 8)
+  	 @updated = current_user.get_voted Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 12)
     end
   end
   def popular
-  	@updatess = Novel.paginate(page: params[:page], :per_page => 8)
+  	@updates = Novel.paginate(page: params[:page], :per_page => 12)
   end
   def newest
-   	@updater = Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 8)
+   	@updates = Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 12)
   end
 end
