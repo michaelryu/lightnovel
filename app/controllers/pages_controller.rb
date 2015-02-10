483e45c775213a063eb6a6b7be1a350b5222f85e
Michael Ryu <michaelryu0@gmail.com>
2015-02-10 04:27:33 -0800

    added reorder

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,13 @@ class PagesController < ApplicationController
   def home
     @unovel=  Novel.includes(:chapters).order("chapters.created_at desc") 
     if user_signed_in?
-  	 @updated = current_user.get_voted Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 8)
+  	 @updated = current_user.get_voted Novel.includes(:chapters).reorder("chapters.created_at desc").paginate(page: params[:page], :per_page => 8)
     end
   end
   def popular
   	@popular = Novel.paginate(page: params[:page], :per_page => 8)
   end
   def newest
-   	@latest = Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 8)
+   	@latest = Novel.includes(:chapters).reorder("chapters.created_at desc").paginate(page: params[:page], :per_page => 8)
   end
 end
