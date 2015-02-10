0a40d55f186c875a581d7e14b1499f4382489d17
Michael Ryu <michaelryu0@gmail.com>
2015-02-09 20:21:26 -0800

    more intuitive names, still no result with showing correct results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,13 @@ class PagesController < ApplicationController
   def home
     @unovel=  Novel.includes(:chapters).order("chapters.created_at desc") 
     if user_signed_in?
-  	 @updated = current_user.get_voted Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 12)
+  	 @updated = current_user.get_voted Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 8)
     end
   end
   def popular
-  	@updates = Novel.paginate(page: params[:page], :per_page => 12)
+  	@popular = Novel.paginate(page: params[:page], :per_page => 8)
   end
   def newest
-   	@updates = Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 12)
+   	@latest = Novel.includes(:chapters).order("chapters.created_at desc").paginate(page: params[:page], :per_page => 8)
   end
 end
