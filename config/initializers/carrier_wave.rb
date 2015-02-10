162415ddcab56cbb3d1d4feaf3114b573fbef8fb
Michael Ryu <michaelryu0@gmail.com>
2015-02-09 18:25:21 -0800

    added path style

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
 			# Configuration for Amazon S3
 			:provider => 'AWS',
 			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
-			:aws_secret_access_key => ENV['S3_SECRET_KEY']
+			:aws_secret_access_key => ENV['S3_SECRET_KEY'],
+			:path_style => true
 		}
 		config.fog_directory = ENV['S3_BUCKET']
-		storage = Fog::Storage.new(credentials.merge({:region => DEFAULT_REGION}))
 	end
 end
