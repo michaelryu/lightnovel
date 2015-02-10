d450dcff8523a1288efbee850f985a9830bf7b62
Michael Ryu <michaelryu0@gmail.com>
2015-02-10 00:43:59 -0800

    changed migration for votes

--- db/migrate/20150207222523_add_cached_votes_total_to_novels.rb
+++ db/migrate/20150207222523_add_cached_votes_total_to_novels.rb
@@ -1,5 +1,5 @@
 class AddCachedVotesTotalToNovels < ActiveRecord::Migration
   def change
-    add_column :novels, :cached_votes_total, :integer
+    add_column :novels, :cached_votes_total, :integer, :default => 0
   end
 end
