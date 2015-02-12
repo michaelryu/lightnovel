e8b3431aa55cf0214b82fbb72c78c040e05fb988
Michael Ryu <michaelryu0@gmail.com>
2015-02-11 21:39:36 -0800

    line wrap for summaries

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,14 @@
 //= require turbolinks
 //= require_tree .
 $( document ).ready(function() {
-// Cache the elements we'll need
-var menu = $('#cssmenu');
-var menuList = menu.find('ul:first');
-var listItems = menu.find('li').not('#responsive-tab');
-
-// Create responsive trigger
-menuList.prepend('<li id="responsive-tab"><a href="#">Menu</a></li>');
-
-// Toggle menu visibility
-menu.on('click', '#responsive-tab', function(){
-	listItems.slideToggle('fast');
-	listItems.addClass('collapsed');
-});
+	// Cache the elements we'll need
+	var menuList = menu.find('ul:first');
+	var listItems = menu.find('li').not('#responsive-tab');
+	// Create responsive trigger
+	menuList.prepend('<li id="responsive-tab"><a href="#">Menu</a></li>');
+	// Toggle menu visibility
+	menu.on('click', '#responsive-tab', function(){
+		listItems.slideToggle('fast');
+		listItems.addClass('collapsed');
+	});
 });
